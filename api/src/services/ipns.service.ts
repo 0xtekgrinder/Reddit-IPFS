df633e44a4f74be4249f69e501167fe2e51c9940
0xtekgrinder <0xtekgrinder@protonmail.com>
2022-12-25 23:00:33 +0100

    fix: ipns service now works

--- api/src/services/ipns.service.ts
+++ api/src/services/ipns.service.ts
@@ -10,21 +10,21 @@ export default class IPNSService extends IPFSService{
         for await (const chunk of res) {
             content = [...content, ...chunk];
         }
-        const path = Buffer.from(content.join("")).toString('utf8')
+        const path = Buffer.from(content.join("")).toString('utf8');
         return await IPFSService.retrieveFile<ContentType>(path);
     }
 
-    protected static async storeRecordFile(recordName: string, content: any): Promise<string> {
+    protected static async storeRecordFile(recordName: string, content: any, create: boolean = true): Promise<string> {
         const cid = await IPFSService.storeFile(content);
 
-        await ipfs.key.gen(recordName, {type: 'rsa', size: 2048});
+        if (create) await ipfs.key.gen(recordName, {type: 'rsa', size: 2048});
         const published = await ipfs.name.publish('/ipfs/' + cid, { key: recordName });
         return published.name;
     }
 
-    protected static async appendToList(cidListRecordName: string, cidToAdd: CID): Promise<void> {
+    protected static async appendToList(cidListRecordName: string, cidListRecordKey: string, cidToAdd: CID, createKey: boolean = true): Promise<void> {
         const list = await this.retrieveFile<CID[]>(cidListRecordName);
         list.push(cidToAdd);
-        await this.storeRecordFile(cidListRecordName, list);
+        await this.storeRecordFile(cidListRecordKey, list, createKey);
     }
 }
