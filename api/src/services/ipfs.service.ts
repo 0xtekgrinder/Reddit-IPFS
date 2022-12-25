0bb3a2de53a87a25851d125efcf3268622637476
0xtekgrinder <0xtekgrinder@protonmail.com>
2022-12-25 22:21:58 +0100

    fix: can now retrieve a file

--- api/src/services/ipfs.service.ts
+++ api/src/services/ipfs.service.ts
@@ -3,15 +3,15 @@ import { CID } from "../types/CID.js";
 
 export default class IPFSService {
     protected static async retrieveFile<ContentType>(cid: CID): Promise<ContentType> {
-        const res = await ipfs.cat(cid);
+        const res = ipfs.cat(cid);
         let content = [];
 
         for await (const chunk of res) {
             content = [...content, ...chunk];
         }
-        const raw = Buffer.from(content).toString('utf8')
+        const raw = Buffer.from(content).toString('utf8');
         const file = JSON.parse(raw);
-        return file.data as ContentType;
+        return file as ContentType;
     }
 
     protected static async storeFile(content: any): Promise<CID> {
